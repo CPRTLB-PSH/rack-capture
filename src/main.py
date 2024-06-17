4686850ce507f98486d1cc6303967b09e1aba434
TheIcyStar <TheIcyStar@users.noreply.github.com>
2024-06-16 20:17:00 -0400

    wip 2

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import threading
+import os
 from picamWrapper import take_image
 import util
 
 config_data = util.get_config()
 
+def data_snapshot():
+    path = os.path.join(util.get_env("UPLOAD_DIR"),)
 
 # Set up capture loop
-capture_timer = threading.Timer(util.string_to_seconds(config_data.interval))
+capture_timer = threading.Timer(util.string_to_seconds(config_data.interval), data_snapshot)
 
 
 
--- src/util.py
+++ src/util.py
@@ -14,5 +14,35 @@ except FileNotFoundError as e:
 def get_config():
     return config_data
 
-def string_to_seconds(string: str): #type hints pls
-    return 300
+def get_env(env_name: str) -> str:
+    """
+    Act as a wrapper for getting environment variables. Uses default values if nothing is set
+    """
+    if os.environ[env_name]:
+        return os.environ[env_name]
+    else:
+        if env_name == "UPLOAD_DIR":
+            return "./data/uploads"
+        elif env_name == "CACHE_DIR":
+            return "./data"
+        elif env_name == "CONFIG_PATH":
+            return "./config.json"
+
+
+def string_to_seconds(string: str) -> int:
+    """
+    Takes in a string, like "3d", or "40h", and turns it into seconds
+    m = minutes, h = hours, d = days, "w" = weeks
+    """
+    unit = string[-1]
+    quantity = int(string[:-1])
+    if unit == "m":
+        return quantity * 60
+    elif unit == "h":
+        return quantity * 60 * 60
+    elif unit == "d":
+        return quantity * 60 * 60 * 24
+    elif unit == "w":
+        return quantity * 60 * 60 * 24 * 7
+    else:
+        raise ValueError(f"string_to_seconds(): {string} is not in the correct format")
